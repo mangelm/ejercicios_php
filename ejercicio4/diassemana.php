fab54a5b185797ab65d3c543ecae964437d170cc
Miguel Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-09-20 10:42:15 +0200

    exercise 4

---
+++ ejercicio4/diassemana.php
@@ -0,0 +1,33 @@
+<?php
+
+/* 
+ Diseña un formulario que pida al usuario un número del 1 al 7. 
+Al enviar el formulario, el programa debe retornar el día de 
+la semana correspondiente (ejemplo: 1 = Lunes, 2 = Martes, etc.). 
+Si el número está fuera de ese rango, muestra un mensaje de error. */
+
+$day = $_POST['day'];
+
+
+$days = [
+    1 => "Lunes",
+    2 => "Martes",
+    3 => "Miércoles",
+    4 => "Jueves",
+    5 => "Viernes",
+    6 => "Sábado",
+    7 => "Domingo"
+];
+
+// If the key exists do the if thing
+if (array_key_exists($day,$days)){
+    echo "Has puesto el número $day, eso significa que el dia es: $days[$day]";
+}
+else{
+    echo "Ese dia no esta en el rango, debe estar entre 1 y 7";
+};
+
+?>
+
+
+<?php
