4206dd808bab92256a4216060c8d4555e41561da
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-10-04 08:45:35 +0200

    ejercicio 7 corregido

--- ejercicio7/tabla_multiplicar.php
+++ ejercicio7/tabla_multiplicar.php
@@ -1,19 +1,36 @@
 <?php
 
 /* 
- Crea un formulario que solicite al usuario un número entre 1 y 10.
+Crea un formulario que solicite al usuario un número entre 1 y 10.
   Al enviar el formulario, el programa debe mostrar la tabla de 
   multiplicar de ese número. */
 
-$n = $_POST['n'];
+// $n = $_POST['n'];
 
-for ($i=0; $i <=10 ; $i++) { 
-    $resultado = $n * $i;
+// for ($i=0; $i <=10 ; $i++) { 
+//     $resultado = $n * $i;
 
-    echo "$n x $i = $resultado <br>";
+//     echo "$n x $i = $resultado <br>";
+// }
+
+
+//otra forma
+if (isset($_POST['n'])) {
+  $n = $_POST['n'];
+
+  // Verificar si es un número entre 1 y 10
+  if (is_numeric($n)) {
+      echo "<h3>Tabla de multiplicar del $n</h3>";
+      echo "<table border='1' cellpadding='5'>";
+      for ($i = 1; $i <= 10; $i++) {
+          $mult = $n * $i;
+          echo "<tr>";
+          echo "<td>$n x $i = $mult</td>";
+          echo "</tr>";
+      }
+      echo "</table>";
+  }
 }
-
-
 
 ?>
 
